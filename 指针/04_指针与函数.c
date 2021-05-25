bbb3044129b4d701cba84b0d92378ab64c08962b
yahaha <596000428@qq.com>
2021-05-25 10:35:18 +0800

    指针与函数

--- 指针/04_指针与函数.c
+++ 指针/04_指针与函数.c
@@ -16,9 +16,88 @@
 //剩下什么类型就保存什么类型数据的地址
 //p = &a;
 
+void swap(int x, int y)
+{
+	int k = 0;
+
+	k = x;
+	x = y;
+	y = k;
+	printf("x = %d  y = %d\n",x ,y);
+
+	return;
+}
+
+
+void swap2(int* x, int* y)
+{
+	int k = 0;
+
+	k = *x;
+	*x = *y;
+	*y = k;
+	printf("x = %d  y = %d\n", *x, *y);
+
+	return;
+}
+
+/*
+int main01()
+{
+	int a = 20;
+	int b = 30;
+
+	// swap(a, b);
+	swap2(&a, &b);
+	printf("a = %d  b = %d\n", a, b);
+
+	system("pause");
+	return 0;
+}
+*/
+
+// 函数传入数组,数组会退化为指针
+void print_arr(int *b, int len) // int *b
+{
+	int line = sizeof(b) / sizeof(b[0]); // b[0] == *(b+0)
+
+	for (int i = 0; i < len; i++)
+	{
+		printf("%d ", b[i]);
+	}
+	printf("\n");
+
+	return;
+}
+
+
+int main02()
+{
+	int a[10] = { 1,2,3,4,5,6,7,8,9,10 };
+
+	print_arr(a, sizeof(a) / sizeof(a[0]));
+
+	system("pause");
+	return 0;
+}
+
+int num = 0; // 全局变量，到程序结束才会释放空间
+
+int* getnum()
+{
+	// {}里面定义的变量为局部变量，局部变量在函数结束后的空间会被释放
+	srand(time(NULL));
+	num = rand();
+
+	return &num;
+}
+
+
 int main()
 {
-	
+	int* p = getnum();
+
+	printf("%d \n", *p);
 
 	system("pause");
 	return 0;
